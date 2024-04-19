a27400c7f00dae0159ebaba5dc872d89f804fe4c
gochi_AI <yumemi1204@protonmail.com>
2024-04-19 19:20:27 +0900

    v

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
+
 var main = document.getElementById('main');
 var canvas = document.createElement('canvas');
 var video = document.createElement('video');
-
-class set_header {
-    constructor() {
-        this.header = document.createElement('header');
-        main.insertAdjacentElement('beforebegin', this.header);
-    }
-    set_header() {
-        //footerには二種類のボタンとサービスロゴを追加する。css内でflexしているので、横に並ぶ。
-        //手前から設定ボタン,サービスロゴ,ヘルプボタン
-        var service_logo = document.createElement('img');
-        service_logo.src = './usagi_AR.svg';
-        
-
-        this.header.appendChild(new make_button('material-symbols:settings', () => this.open_setting()).make_button());
-        this.header.appendChild(service_logo);
-        this.header.appendChild(new make_button('material-symbols:help', () => { }).make_button());
-    }
-    open_setting() {
-        //create setting
-        var setting = document.createElement('div');
-        setting.id = 'setting';
-        setting.style.width = window.innerWidth  + 'px';
-        setting.style.height = window.innerHeight + 'px';
-        //create setting content
-        var setting_content = document.createElement('div');
-        setting_content.id = 'setting_content';
-        setting.appendChild(setting_content);
-        setting.appendChild(new make_button('material-symbols:close', () => setting.remove()).make_button());
-        main.appendChild(setting);
-
-    }
+async function addBA(id,func){
+    await document.getElementById(id).addEventListener('click',function(){
+        func();
+    });
 }
+addBA('button_setting',()=>{fetch_page('start.html');});
+addBA('button_help',()=>{fetch_page('help.html');});
 class set_cAv {
     constructor() {
         this.canvas = canvas;
@@ -65,61 +41,49 @@ class set_cAv {
     }
 
 }
-class set_footer {
-    constructor() {
-        this.footer = document.createElement('footer');
-        main.insertAdjacentElement('afterend', this.footer);
+
+class chara_sprite{
+    constructor(){
+        this.x = 0;
+        this.y = 0;
+        this.width = 0;
+        this.height = 0;
+        this.src = '';
+        this.sprite_name = '';
     }
-    set_footer() {
-        //footerには三種類のボタンを追加する。css内でflexしているので、横に並ぶ。
-        //手前から画像操作ボタン,撮影ボタン,カメラ切り替えボタン
-        this.footer.appendChild(new make_button('material-symbols:person-edit-rounded', () => { }).make_button());
-        this.footer.appendChild(new make_button('material-symbols:camera',  () => this.take_photo()).make_button());
-        this.footer.appendChild(new make_button('material-symbols:flip-camera-android', () =>  this.switch_camera()).make_button());
+    set_chara_sprite(x,y,width,height,src,sprite_name){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.src = src;
+        this.sprite_name = sprite_name;
     }
-    switch_camera() {
-        if (cAv.CONSTRAINTS.video.facingMode == 'user') {
-            cAv.CONSTRAINTS.video.facingMode = 'environment';
-        } else {
-            cAv.CONSTRAINTS.video.facingMode = 'user';
-        }   
-}
-take_photo() {
-    var ctx = cAv.canvas.getContext('2d');
-    ctx.drawImage(cAv.video, 0, 0, cAv.device_width, cAv.device_height);
-    var img = cAv.canvas.toDataURL('image/png');
-
-}
-}
-
-class make_button {
-    constructor(src, func) {
-        this.button = document.createElement('button');
-        this.button.type = 'button';
-        //srcにはiconifyのアイコンのnameを入れる
-        let icon = document.createElement('iconify-icon');
-        icon.setAttribute('icon', src);
-        this.button.appendChild(icon);
-        this.button.src = src;
-        this.button.addEventListener('click', func);
+    load_chara_config(sprite_name){
+        datum=localStorage.getItem("sprites_config");
+        this.sprite_name = sprite_name;
+        if(datum){
+            datum=JSON.parse(datum);
+            if(datum[this.sprite_name]){
+                this.x = datum[this.sprite_name].x;
+                this.y = datum[this.sprite_name].y;
+                this.width = datum[this.sprite_name].width;
+                this.height = datum[this.sprite_name].height;
+                this.src = datum[this.sprite_name].src;
+            }
+        }
     }
-    make_button() {
-        return this.button;
-    }
+}
+
+function fetch_page(url){
+    fetch(url)
+    .then(response => response.text())
+    .then(data => {
+        main.innerHTML = data;
+    });
 
 }
-var cAv = new set_cAv();
-cAv.get_device_size();
-cAv.set_canvas();
-cAv.set_video();
-
-var header = new set_header();
-header.set_header();
-var footer = new set_footer();
-footer.set_footer();
-
-window.addEventListener('resize', () => {
-    cAv.get_device_size();
-    cAv.set_canvas();
-    cAv.set_video();
-});
+set_cAv = new set_cAv();
+set_cAv.get_device_size();
+set_cAv.set_canvas();
+set_cAv.set_video();
