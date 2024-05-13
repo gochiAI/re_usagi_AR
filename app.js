69053ae4fe2ad35cb905b1ec864859deb3354f54
gochi_AI <yumemi1204@protonmail.com>
2024-05-13 20:18:39 +0900

    [add,fix,remove]add_sprite,Class

--- app.js
+++ app.js
@@ -22,22 +22,25 @@ class LocalStorageUtility {
 }
 
 var CopilotLS = new LocalStorageUtility();
-
-var CharacterDatas = ["alarm/chino_0", "alarm/chiya_0", "alarm/cocoa_0", "alarm/rize_0", "alarm/syaro_0",
-    "lessar/chino_0", "lessar/chiya_0", "lessar/cocoa_0", "lessar/rize_0", "lessar/syaro_0", "lessar/maya_0", "lessar/megu_0", "lessar/fuyu_0",
-    "ekimemo/chino_usual", "ekimemo/chino_smile", "ekimemo/chino_angry", "ekimemo/chino_dovey", "ekimemo/chino_tired",
-    "ekimemo/chiya_usual", "ekimemo/chiya_smile", "ekimemo/chiya_angry", "ekimemo/chiya_dovey", "ekimemo/chiya_tired",
-    "ekimemo/cocoa_usual", "ekimemo/cocoa_smile", "ekimemo/cocoa_angry", "ekimemo/cocoa_dovey", "ekimemo/cocoa_tired",
-    "ekimemo/rize_usual", "ekimemo/rize_smile", "ekimemo/rize_angry", "ekimemo/rize_dovey", "ekimemo/rize_tired",
-    "ekimemo/syaro_usual", "ekimemo/syaro_smile", "ekimemo/syaro_angry", "ekimemo/syaro_dovey", "ekimemo/syaro_tired",
-    "ekimemo/maya_usual", "ekimemo/maya_smile", "ekimemo/maya_angry", "ekimemo/maya_dovey", "ekimemo/maya_tired",
-    "ekimemo/megu_usual", "ekimemo/megu_smile", "ekimemo/megu_angry", "ekimemo/megu_dovey", "ekimemo/megu_tired",
-];
-
 var main = document.getElementById('main');
 var canvas = document.createElement('canvas');
 var video = document.createElement('video');
+class system_data {
+    CharacterDatas = ["alarm/chino_0", "alarm/chiya_0", "alarm/cocoa_0", "alarm/rize_0", "alarm/syaro_0",
+        "lessar/chino_0", "lessar/chiya_0", "lessar/cocoa_0", "lessar/rize_0", "lessar/syaro_0", "lessar/maya_0", "lessar/megu_0", "lessar/fuyu_0",
+        "ekimemo/chino_usual", "ekimemo/chino_smile", "ekimemo/chino_angry", "ekimemo/chino_dovey", "ekimemo/chino_tired",
+        "ekimemo/chiya_usual", "ekimemo/chiya_smile", "ekimemo/chiya_angry", "ekimemo/chiya_dovey", "ekimemo/chiya_tired",
+        "ekimemo/cocoa_usual", "ekimemo/cocoa_smile", "ekimemo/cocoa_angry", "ekimemo/cocoa_dovey", "ekimemo/cocoa_tired",
+        "ekimemo/rize_usual", "ekimemo/rize_smile", "ekimemo/rize_angry", "ekimemo/rize_dovey", "ekimemo/rize_tired",
+        "ekimemo/syaro_usual", "ekimemo/syaro_smile", "ekimemo/syaro_angry", "ekimemo/syaro_dovey", "ekimemo/syaro_tired",
+        "ekimemo/maya_usual", "ekimemo/maya_smile", "ekimemo/maya_angry", "ekimemo/maya_dovey", "ekimemo/maya_tired",
+        "ekimemo/megu_usual", "ekimemo/megu_smile", "ekimemo/megu_angry", "ekimemo/megu_dovey", "ekimemo/megu_tired",
+    ];
 
+    Events = ['全て', 'ekimemo', 'alarm', 'lessar'];
+
+    Character = ['全て', 'chino', 'cocoa', 'rize', 'chiya', 'syaro', 'maya', 'megu', 'fuyu'];
+}
 class set_cAv {
     constructor() {
         this.canvas = canvas;
@@ -69,7 +72,7 @@ class set_cAv {
                 width: { ideal: this.video.width },
                 height: { ideal: this.video.height },
                 aspectRatio: { ideal: 16 / 9 },
-                facingMode: null,
+                facingMode: "user",
             },
         };
 
@@ -77,15 +80,16 @@ class set_cAv {
     }
 }
 class video_stream {
-    constructor(video, CONSTRAINTS) {
-        this.video = video;
-        this.CONSTRAINTS = CONSTRAINTS;
+    constructor() {
+        this.is_front = true;
+        this.video = new set_cAv().video;
+        this.CONSTRAINTS = new set_cAv().CONSTRAINTS || { video: {} };
         this.curSTREAM = null;
         this.initCamera();
         this.syncCamera();
     }
-    syncCamera(is_front = false) {
-        this.CONSTRAINTS.video.facingMode = is_front
+    syncCamera() {
+        this.CONSTRAINTS.video.facingMode = this.is_front
             ? "user"
             : { exact: "environment" };
 
@@ -162,38 +166,134 @@ class get_src {
     }
 }
 
-// 名前空間を作成
-var App = {};
-
-App.editSprite = function (button, sprite_name) {
-    if (button.classList.contains("selected")) {
-        button.classList.remove("selected");
-        console.log("Unselected Sprite:", sprite_name);
-    } else {
-        document.querySelectorAll("#tab_sprite button").forEach((btn) => {
-            btn.classList.remove("selected");
+class set_ASW {
+    constructor() {
+        this.CharacterDatas = new system_data().CharacterDatas;
+        this.Events = new system_data().Events;
+        this.Character = new system_data().Character;
+        this.eventFilter = document.getElementById('Event_filter');
+        this.characterFilter = document.getElementById('Character_filter');
+        this.optionCreates(this.eventFilter, this.Events);
+        this.optionCreates(this.characterFilter, this.Character);
+    }
+    optionCreates = (f, value) => {
+        const option = document.createElement('option');
+        value.forEach((v) => {
+            option.value = v;
+            option.text = v;
+            f.appendChild(option);
         });
-        button.classList.add("selected");
-        console.log("Selected Sprite:", sprite_name);
+    }
+    filterList = () => {
+        const selectedEvent = this.eventFilter.value;
+        const selectedCharacter = this.characterFilter.value;
+
+        const filterList = CharacterDatas.filter(sprite_name => {
+            if (selectedEvent === "全て" && selectedCharacter === "全て") return true;
+            if (selectedEvent === "全て") return sprite_name.includes(selectedCharacter);
+            if (selectedCharacter === "全て") return sprite_name.includes(selectedEvent);
+            return sprite_name.includes(selectedEvent) && sprite_name.includes(selectedCharacter);
+        });
+
+        parentDiv.innerHTML = '';
+        this.displayCh(filterList);
+    }
+    createButton = (iconSrc, onClick) => {
+        const button = document.createElement('button');
+        const icon = document.createElement('img');
+        icon.src = iconSrc;
+        button.appendChild(icon);
+        button.onclick = onClick;
+        return button;
+    }
+    displayCh = (CharacterDatas) => {
+        CharacterDatas.forEach(sprite_name => {
+            const newDiv = document.createElement('div');
+            newDiv.classList.add("SpriteCard");
+
+            const img = document.createElement('img');
+            img.src = new get_src().get_thumb_src(sprite_name);
+
+            newDiv.appendChild(img);
+
+            const CardButtons = document.createElement('div');
+            CardButtons.id = "CardButtons";
+
+            const imgZoom_Button = createButton("./core_sys/img/ui/img_zoom.svg", function () {
+                const Img_Modal = document.createElement('div');
+                Img_Modal.id = "Img_Modal";
+                Img_Modal.onclick = function () { Img_Modal.remove(); };
+                const img = document.createElement('img');
+                img.src = new get_src().get_stand_src(sprite_name);
+                Img_Modal.appendChild(img);
+                document.body.appendChild(Img_Modal);
+            });
+
+            const selectSprite_Button = createButton(CopilotLS.getStorage(sprite_name) ? "./core_sys/img/ui/user_remove.svg" : "./core_sys/img/ui/user_add.svg", function () {
+                const selectSprite_icon = this.firstChild; // Add this line
+                if (newDiv.classList.contains('selected')) {
+                    selectSprite_icon.src = "./core_sys/img/ui/user_add.svg";
+                    CopilotLS.removeData(sprite_name);
+                    newDiv.classList.remove('selected');
+                } else {
+                    selectSprite_icon.src = "./core_sys/img/ui/user_remove.svg";
+                    CopilotLS.setStorage(sprite_name, { "X": 0, "Y": 0, "ZoomRate": 1.0 });
+                    newDiv.classList.add('selected');
+                }
+            });
+
+            if (CopilotLS.getStorage(sprite_name)) newDiv.classList.add('selected');
+
+            CardButtons.appendChild(imgZoom_Button);
+            CardButtons.appendChild(selectSprite_Button);
+            newDiv.appendChild(CardButtons);
+
+            parentDiv.appendChild(newDiv);
+        });
+
     }
 }
 
-App.createButtonWithImage = function (imagePath, sprite_name) {
-    return `
-    <button onclick="App.editSprite(this,'${sprite_name}')">
-        <img id="spriteImage" src="${imagePath}" alt="sprite" />
-    </button>
-`;
+//各ボタンに対応する処理を追加
+document.getElementById('button_help_close').onclick = function () {
+    document.getElementById('help').classList.remove('show');
+}
+document.getElementById('button_help').onclick = function () {
+    document.getElementById('help').classList.add('show');
+}
+document.getElementById('button_edit').onclick = function () {
+    if (document.getElementById('edit_tab').classList.contains('show')) {
+        document.getElementById('edit_tab').classList.remove('show');
+    }
+    else {
+        document.getElementById('edit_tab').classList.add('show');
+    }
 }
 
-CopilotLS.getAllKeys().forEach((sprite_name) => {
-    const thumbSrc = new get_src().get_thumb_src(sprite_name);
-    const button = App.createButtonWithImage(thumbSrc, sprite_name);
-    document.getElementById("tab_sprite").innerHTML += button;
-});
-
-
-
-
-var standby = new set_cAv();
-var videoStream = new video_stream(standby.video, standby.CONSTRAINTS);
+let s_cAv = new set_cAv();
+let m_video = new video_stream();
+document.getElementById('button_flip').onclick = function () {
+    m_video.is_front = !m_video.is_front;
+    m_video.syncCamera();
+    m_video.initCamera();
+}
+document.getElementById('button_camera').onclick = function () {
+    //canvasとvideoをそれぞれ一時的なcanvasに描画
+    const tempCanvas = document.createElement('canvas');
+    tempCanvas.width = s_cAv.canvas.width;
+    tempCanvas.height = s_cAv.canvas.height;
+    const tempCtx = tempCanvas.getContext('2d');
+    tempCtx.drawImage(s_cAv.video, 0, 0, s_cAv.canvas.width, s_cAv.canvas.height);
+    tempCtx.drawImage(s_cAv.canvas, 0, 0, s_cAv.canvas.width, s_cAv.canvas.height);
+    //一時的なcanvasを画像に変換
+    const img = new Image();
+    img.src = tempCanvas.toDataURL("image/png");
+    //一時的なcanvasを削除
+    tempCanvas.remove();
+    //画像をモーダルで表示
+    const Img_Modal = document.createElement('div');
+    Img_Modal.id = "Img_Modal";
+    Img_Modal.onclick = function () { Img_Modal.remove(); };
+    Img_Modal.appendChild(img);
+    document.body.appendChild(Img_Modal);
+    }
